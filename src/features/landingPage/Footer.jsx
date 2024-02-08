e3cd24976d079b8074b78664a576ef48a5e541f5
Naresh M <naresh2004.m@gmail.com>
2024-02-08 20:22:06 +0530

    added .env to gitignore

--- src/features/landingPage/Footer.jsx
+++ src/features/landingPage/Footer.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
+
 function Footer() {
   return (
     <div className="flex items-center justify-center bg-gray-500 p-3">
       <p className="inline-block text-center">
-        Copyrights reserved &copy;2024. Designed and Developed by Naresh M
+        Copyrights reserved &copy;2024. Designed and Developed by{" "}
+        <Link
+          to="https://nareshdev.netlify.app/"
+          className="text-main_neon"
+          target="_blank"
+        >
+          Naresh M
+        </Link>
       </p>
     </div>
   );
