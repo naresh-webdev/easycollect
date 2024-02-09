d537166605a143603527a042bfcf3ddce0435c86
Naresh M <naresh2004.m@gmail.com>
2024-02-09 15:48:00 +0530

    minor change in styling of landing page fixd

--- src/features/landingPage/Title.jsx
+++ src/features/landingPage/Title.jsx
@@ -1,6 +1,6 @@
 function Title({ children }) {
   return (
-    <h3 className="mb-6 pt-20 text-center text-xl uppercase tracking-wide text-main_neon sm:pt-28 sm:text-2xl   md:text-3xl md:font-semibold lg:pt-24 lg:text-4xl">
+    <h3 className="mb-6 pt-8 text-center text-xl uppercase tracking-wide text-main_neon min-[250px]:pt-14 min-[400px]:pt-14 sm:pt-28 sm:text-2xl   md:text-3xl md:font-semibold lg:pt-24 lg:text-4xl">
       {children}
     </h3>
   );
