18d6a007d11ee3034b41e9fdad34e16a0854e366
Naresh M <naresh2004.m@gmail.com>
2024-02-09 17:01:57 +0530

    removed all cached data

--- src/features/landingPage/Title.jsx
+++ src/features/landingPage/Title.jsx
@@ -1,6 +1,6 @@
 function Title({ children }) {
   return (
-    <h3 className="mb-6 pt-6 text-center text-xl uppercase tracking-wide text-main_neon min-[250px]:pt-10 min-[400px]:pt-14 sm:pt-28 sm:text-2xl   md:text-3xl md:font-semibold lg:pt-24 lg:text-4xl">
+    <h3 className="mb-6 pt-4 text-center text-xl uppercase tracking-wide text-main_neon min-[250px]:pt-8 min-[400px]:pt-14 sm:mb-8 sm:pt-24 sm:text-2xl   md:text-3xl md:font-semibold lg:pt-24 lg:text-4xl">
       {children}
     </h3>
   );
