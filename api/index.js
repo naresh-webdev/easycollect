364dc3c265a5616bce5c183a297e2d6c788ea0eb
Naresh M <naresh2004.m@gmail.com>
2024-04-22 07:40:58 +0530

    deploy prob-3

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ app.use("/api/payment", paymentRoute);
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+  return res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 app.use((error, req, res, next) => {
